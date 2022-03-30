5b70d5b127637a88d2f44c00ea09047f88695be4
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-03-29 18:29:41 -0700

    delete and edit

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ const editForm = (data) => {
     <Def>
       <main>
         <h1>Add New Place</h1>
-        <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
           <div className="row">
             <div className="form-group col-sm-6">
               <label htmlFor="name">Place Name</label>
@@ -17,6 +17,15 @@ const editForm = (data) => {
                 defaultValue={data.place.name}
                 required
               />
+              <div className="form-group col-sm-6">
+                <label htmlFor="founded">Founded</label>
+                <input 
+                type="number" 
+                className="form-control"
+                id="founded"
+                name="founded"
+                value={data.place.founded} />
+              </div>
             </div>
             <div className="form-group col-sm-6">
               <label htmlFor="pic">Place Picture</label>
@@ -59,7 +68,7 @@ const editForm = (data) => {
               required
             />
           </div>
-          <input className="btn btn-primary" type="submit" value="Edit Place" />
+          <input className="btn btn-primary" type="submit" value="Update Place" />
         </form>
       </main>
     </Def>
