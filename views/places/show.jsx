f4d3c10f7b5f9e985efe9e7da59f6baca2803324
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-03-31 15:55:53 -0700

    pushing the project on mongo

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -12,18 +12,26 @@ function show(data) {
       Not yet rated
     </h3>
   )
+if(data.place.comments.length) {
+
+  comments = data.place.comments.map(c => {
+    return (
+      <div className="border">
+        <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+        <h4>{c.content}</h4>
+        <h3>
+          <stong>- {c.author}</stong>
+        </h3>
+        <h4>Rating: {c.stars}</h4>
+      </div>
+    )
+  })
+}
+  
+
+  
   if (data.place.comments.length) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
-      // return (
-      //   <div className="border">
-      //     <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-      //     <h4>{c.content}</h4>
-      //     <h3>
-      //       <stong>- {c.author}</stong>
-      //     </h3>
-      //     <h4>Rating: {c.stars}</h4>
-      //   </div>
-      // )
       return tot + c.stars
     }, 0)
     let averageRating = Math.round(sumRatings / data.place.comments.length)
